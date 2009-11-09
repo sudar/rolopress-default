ed9f0b5ae1e6092c344206968e953de9b94c7e1f
sbruner <sbruner@slipfire.com>
2009-11-09 04:38:50 +0000

    rolopress core and default: some tweaking
    git-svn-id: http://svn.sudarmuthu.com/rolopress/rolopress-default/trunk@78 38a0677f-509c-40db-b594-c9751fff4674

--- functions.php
+++ functions.php
@@ -1,19 +1,21 @@
 <?php
 
 
+/* Create your own menu */
+
 add_action('init', 'remove_rolopress_default_menu'); // first call the function to remove the default menu
 
 function remove_rolopress_default_menu() { // here's where we remove the default menu
       remove_filter('rolopress_menu', 'rolopress_default_menu');
 }
 
-function rp_menu2() { // here's our new menu
+function my_menu() { // here's our new menu
 
     /* Widgetized Menu*/
 					echo "new menu item";
 				}
 
-add_filter('rolopress_menu', 'rp_menu2');
+add_filter('rolopress_menu', 'my_menu');
 
 
 
